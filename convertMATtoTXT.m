fe4a4802b17d24d3d53545c8fb8e5294e0aa3576
Ali Özgür Argunşah <aoargunsah@gmail.com>
2022-08-15 18:52:12 +0200

    PathLineAdded
    Line 5 and 6 added

--- convertMATtoTXT.m
+++ convertMATtoTXT.m
@@ -2,17 +2,20 @@
 
 % Part4: Conversion on InfoNums Files to New Format for Analysis in R (txt - tab separated)
 
+currentDir = pwd;
+addpath(genpath(currentDir));
+
 D1 = uipickfiles; % Select Files in _infoNums_ Folder
 
 for i = 1:size(D1,2)
     load(D1{i});
-    
+
     [path,name,ext] = fileparts(D1{i});
     newBranchLen    = cell2mat(branchLen);
-    
+
     clear M;
     maxSize = 0;
-    
+
     for j = 1:size(individualSizes,1)
         for k = 1:size(individualSizes(j,:),2)
             temp = individualSizes{j,k};
@@ -24,7 +27,7 @@ for i = 1:size(D1,2)
         end
         M = nan(maxSize+1,size(individualSizes(j,:),2));
         M(1,:) = newBranchLen;
-        
+
         for k = 1:size(individualSizes(j,:),2)
             temp = individualSizes{j,k};
             if ~isempty(temp)
@@ -33,6 +36,6 @@ for i = 1:size(D1,2)
         end
     end
     writematrix(M,fullfile(path,sprintf('%s_newFormat.txt',name)),'Delimiter','tab');
-  
+
     Mcell{i} = M;
 end
